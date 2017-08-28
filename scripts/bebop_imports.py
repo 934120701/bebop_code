5563d95ee9812d14e38bfac4e2e01be404282b47
TeamAutonomia <intelligentrobotics2017@gmail.com>
2017-08-28 14:11:36 +0100

    updated for communication

--- scripts/bebop_imports.py
+++ scripts/bebop_imports.py
@@ -7,8 +7,9 @@ import cv2
 import cv2.aruco as aruco
 import glob
 import numpy as np
+import socket
 
-
+from time import sleep
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
